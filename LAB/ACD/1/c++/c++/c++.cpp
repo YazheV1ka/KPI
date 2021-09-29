a56e641d38ed726ec0770dccd552414063d5e92b
YazheV1ka <89256514+YazheV1ka@users.noreply.github.com>
2021-09-29 18:38:44 +0300

    Add files via upload

---
+++ LAB/ACD/1/c++/c++/c++.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    int a,V,S;
+    cin >> a;
+    V=pow(a, 4);
+    S = 4 * pow(a, 2);
+    cout << V<<"\n";
+    cout << S;
+
+}
