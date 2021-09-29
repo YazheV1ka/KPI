03df0805fb530ef91f72b32c3ed5de3a3cd1a305
YazheV1ka <89256514+YazheV1ka@users.noreply.github.com>
2021-09-29 18:10:15 +0300

    Add files via upload

---
+++ LAB/OP/1/c++ op/c++ op/c++ op/c++ op.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Ukr");
+	int num = 0, movnum = 0, staynum = 0, result = 0;
+	cout << "Тризначне число:";
+	cin >> num; //ввод тризначного числа
+	movnum = num / 100; //знаходимо 1 цифру зліва
+	staynum = num % 100; //знаходимо число без 1 цифри зліва
+	result = staynum * 10 + movnum; //дописуємо цифру в кінець
+	cout << "Результат:" << result;
+}
