3d4d15ab8a9e289e2cde88acca7ffbd25f3cb110
YazheV1ka <89256514+YazheV1ka@users.noreply.github.com>
2021-10-12 12:25:35 +0300

    Add files via upload

---
+++ LAB/OP/4/c++ op4/c++ op4/c++ op4.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    const float sin = 0.479426; //sin(0.5)
+    float y = 0, sum = 0;
+    int n = 0, i = 1;
+    cout << "Введите n: ";//кількість ітерацій
+    cin >> n;
+    for (i = 1; i <= n; i++)
+    {
+        y = 1 / (sin / (pow(i, 2)));//загальна формула
+        sum += y;
+        cout << "Результат: " << sum << endl;
+    }
+    cout <<"Финальний результат: " << sum<< endl;
+    
+}
+    
---
+++ LAB/OP/4/op4 py.py
@@ -0,0 +1,15 @@
+sin = 0.479426 #sin(0.5)
+y = 0
+suma = 0
+i = 1
+print("Введіть n: ")
+n = int(input())
+
+while (i <= n):
+    y = 1 / ( sin / ((i**2)))
+    suma += y
+    i+=1
+
+print(suma)
+
+    
