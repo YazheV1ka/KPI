e62620f462f224aa00f59b21db78773052ef546c
YazheV1ka <89256514+YazheV1ka@users.noreply.github.com>
2021-12-08 19:26:44 +0300

    Add files via upload

---
+++ LAB/ACD/7/c++ acd7/c++ acd7/c++ acd7.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char a[10];
+    char b[10];
+    char c[10];
+    int i,count=0;
+    for (i = 0; i < 10; i++)
+    {
+        a[i] = 74 - i;
+        b[i] = 65 + 2 * i;
+    }
+
+    cout << "Common symbols: " << endl;
+    if (a[i] == b[i])
+    {
+        c[i] = a[i];
+        cout << "c: " << c[i]<<endl;
+        if ((int)c[i] < 67)
+        {
+            count++;
+        }
+    }
+    cout << "Num of symbols: " << endl;
+    cout << count;
+}
