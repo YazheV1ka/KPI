e27b3c21fd0d3c70f8011079c77b2f22c62fe95f
YazheV1ka <89256514+YazheV1ka@users.noreply.github.com>
2021-12-08 19:27:45 +0300

    Add files via upload

---
+++ LAB/ACD/6/c++ acd6/c++ acd6/c++ acd6.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+
+float sum(float first, float dif, int n);
+
+int main()
+{
+    float first, dif, result;
+    int n;
+    cout << "Enter first number: ";
+    cin >> first;
+    cout << "Enter difference: ";
+    cin >> dif;
+    cout << "Enter number of numbers: ";
+    cin >> n;
+    result = sum(first, dif, n);
+    cout << "Result: " << result << endl;
+}
+
+float sum(float first, float dif, int n)
+{
+    float sum2 = first;
+    if (n > 1)
+    {
+        sum2 += sum(first + dif, dif, --n);
+    }
+    return sum2;
+}
