203563ab1a3f46300d779dab21f6009c93f2538e
YazheV1ka <89256514+YazheV1ka@users.noreply.github.com>
2022-02-15 12:46:22 +0300

    Add files via upload

---
+++ LAB2/OP/1/csharp lab1/csharp lab1/TextWorker.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace csharp_lab1
+{
+    public class TextWorker
+    {
+        public string GetWord()
+        {
+            Console.CursorLeft = 0;
+            Console.WriteLine("Enter word: ");
+            string word = Console.ReadLine();
+            return word;
+        }
+
+        public string[] CheckWord(string[] text, string wordToFind)
+        {
+            var list = new List<string>();
+            foreach (var line in text)
+            {
+                string[] words = line.Replace("\r", "").Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                foreach (var word in words)
+                {
+                    if (wordToFind == word)
+                    {
+                        list.Add(line);
+                        break;
+                    }
+                }
+            }
+            return list.ToArray();
+        }
+    }
+}
