505b37a7a32a44cb9db3af9710b09ca0da4d7cb2
YazheV1ka <89256514+YazheV1ka@users.noreply.github.com>
2022-03-08 19:23:16 +0300

    Add files via upload

---
+++ LAB2/OP/3/csharp lab3/csharp lab3/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+
+namespace csharp_lab3
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter number of texts:");
+            var numOfText = Int32.Parse(Console.ReadLine());
+            
+            TextWorker[] text = new TextWorker[numOfText];
+
+            for (int i = 0; i < numOfText; i++)
+            {
+                text[i] = new TextWorker(i);
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Choose index of text to add string:");
+            var indOfText = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Add string:");
+            text[indOfText-1].InputText(Console.ReadLine());
+            Console.WriteLine();
+
+
+            for (int i = 0; i < numOfText; i++)
+            { 
+                Console.WriteLine($"Text {i+1}:");
+                text[i].OutputText();
+                Console.WriteLine($"Persent of symbols/numbers: {text[i].CheckPersent()}");
+                Console.WriteLine();
+            }
+            
+
+            double minPersent = text[0].CheckPersent();
+            int minInd = 0;
+            
+            for (int i = 0; i < numOfText; i++)
+            {
+                if (minPersent > text[i].CheckPersent())
+                {
+                    minPersent = text[i].CheckPersent();
+                    minInd = i;
+                }
+            }
+            Console.WriteLine($"Text with smallest persent {minInd+1}");
+        }
+    }
+}
---
+++ LAB2/OP/3/csharp lab3/csharp lab3/TextWorker.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace csharp_lab3
+{
+    public class TextWorker
+    {
+        List<string> text = new List<string>();
+        string str = string.Empty;
+
+        public TextWorker(int i)
+        {
+            Console.WriteLine($"Enter string to text {i+1}:");
+            text.Add(Console.ReadLine());
+        }
+
+        
+        public void InputText(string str)
+        {
+            text.Add(str);
+            str = string.Empty;
+        }
+
+        public double CheckPersent()
+        {
+            double alpha = 0;
+            double digit = 0;
+
+            foreach (var line in text)
+            {
+                foreach (char symb in line)
+                {
+                    if (Char.IsNumber(symb))
+                    {
+                        digit++;
+                    }
+                    else
+                    {
+                        alpha++;
+                    }
+
+                }
+            }
+
+            if (digit == 0)
+            {
+                return 100;
+            }
+            else if(alpha == 0)
+            {
+                return 0;
+            }
+
+            return (alpha / digit) * 100;
+        }
+            
+        public void OutputText()
+        {
+            foreach (var line in text)
+            {
+                Console.WriteLine(line);
+            }
+            
+        }
+    }
+}
