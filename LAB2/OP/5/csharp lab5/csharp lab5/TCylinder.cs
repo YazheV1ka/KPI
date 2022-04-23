fdcbf275aa0766cb5027d086796d8f3c6f86be64
YazheV1ka <89256514+YazheV1ka@users.noreply.github.com>
2022-04-23 03:03:20 +0300

    Add files via upload

---
+++ LAB2/OP/5/csharp lab5/csharp lab5/TCylinder.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace csharp_lab5
+{
+    internal class TCylinder : TFigure
+    {
+        private double r, h;
+        public TCylinder(double r, double h)
+        {
+            this.r = r;
+            this.h = h;
+        }
+        public double Vc(double r, double h)
+        {
+            double res = pi * h * Math.Pow(r, 2);
+            return res;
+        }
+        
+        public double getR(){ return r; }
+        public double getH(){ return h; }
+        
+        public string ToStr()
+        {
+            return "Cylinder: " +
+                   $"radius = {r} " +
+                   $"height = {h} " +
+                   $"Volume = {Math.Round(Vc(r, h),3)}";
+        }
+    }
+}
---
+++ LAB2/OP/5/csharp lab5/csharp lab5/TFigure.cs
@@ -0,0 +1,9 @@
+﻿namespace csharp_lab5
+{
+    public class TFigure
+    {
+        public double pi = 3.14;
+        public double Sp(int a, int p){ return 0; }
+        public double Vc(int r, int h){ return 0; }
+    }
+}
